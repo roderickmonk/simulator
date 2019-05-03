8542c283b651b903811ae980b047c059c130932f
roderickmonk <roderickmonk@gmail.com>
2019-05-03 17:05:34 +0000

    gitzap

--- Python/load.py
+++ Python/load.py
@@ -511,11 +511,11 @@ if __name__ == '__main__':
             load_minutes % 60,
             load_time % 60)
 
-        xx = "{0:25}{1:2d}:{2:2d}:{3:2d}".format (
+        xx = "{0:25}{1}:{2}:{3}".format (
             "Load Time (HH:MM:SS):", 
             f'{load_minutes // 60:02}',
-            load_minutes % 60,
-            load_time % 60)
+            f'{load_minutes % 60:02}',
+            f'{load_time % 60:02}')
 
         logging.info (xx)
 
