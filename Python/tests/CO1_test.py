cf9c05a2c9b2a14db44e9b62602d24200f2a28b8
roderickmonk <roderickmonk@gmail.com>
2019-05-26 14:50:39 +0000

    gitzap

--- Python/tests/CO1_test.py
+++ Python/tests/CO1_test.py
@@ -241,9 +241,9 @@ def test_traders_in_real_time():
                         sellob)
 
                     logging.error('local buy_ev:\n%r', trader.buy_ev)
+                    logging.error('remote buy_ev:\n%r', buy_ev_ref)
                     logging.error('local sell_ev:\n%r', trader.sell_ev)
-
-
+                    logging.error('remote sell_ev:\n%r', sell_ev_ref)
                         
                     output_format = "{0:13}{1:10}, {2:6} {3:10}"
                     logging.error(
