b07878b1f7e05ebd2f0383856ee203f672fdb53a
roderickmonk <roderickmonk@gmail.com>
2019-05-26 16:03:09 +0000

    gitzap

--- Python/tests/CO1_test.py
+++ Python/tests/CO1_test.py
@@ -249,9 +249,7 @@ def test_traders_in_real_time():
                         trader.sell_ev.dtype == sell_ev_ref.dtype                       
 
                     logging.fatal("")
-                    logging.error('ev.sizes & shapes the same: %r', EVs_same_structure)
-                    logging.error('local buy_ev.dtype: %r', trader.buy_ev.dtype)
-                    logging.error('remote buy_ev_ref.dtype: %r', buy_ev_ref.dtype)
+                    logging.error('ev.sizes, shapes, and dtypes the same: %r', EVs_same_structure)
 
                     logging.error('local buy_ev: %r', trader.buy_ev)
                     logging.error('remote buy_ev: %r', buy_ev_ref)
