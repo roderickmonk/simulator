a8bf1fab198dc9d5eb49a8e5085158d1ff16c623
roderickmonk <roderickmonk@gmail.com>
2019-07-27 15:48:33 +0000

    gitzap

--- Python/tests/CO1_test.py
+++ Python/tests/CO1_test.py
@@ -392,7 +392,7 @@ def test_compare_bots():
             'priceDepthLimit': bot_config["priceDepthLimit"],
         }
 
-        print ("priceDepthLimit: %f", bot_config["priceDepthLimit"])
+        logging.error ("priceDepthLimit: %f", bot_config["priceDepthLimit"])
 
         logging.error ("Trader: %s", bot_config["trader"])
         if "evol_a_cycler" in bot_config["trader"]:
