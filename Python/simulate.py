0d52df2979adbedc7bf7a19324b29fe1ca5d1bf7
roderickmonk <roderickmonk@gmail.com>
2019-08-01 14:40:53 +0000

    gitzap

--- Python/simulate.py
+++ Python/simulate.py
@@ -118,14 +118,14 @@ def simulate():
                       str(sim_config.partition_config))
 
         # Convenience Destructuring
-        env_id = sim_config.partition_config["envId"]
+        # env_id = sim_config.partition_config["envId"]
         # exchange = sim_config.partition_config["exchange"]
-        market = sim_config.partition_config["market"]
-        fee = sim_config.partition_config['feeRate']
-        QL = sim_config.partition_config['quantityLimit']
-        IL = sim_config.partition_config['inventoryLimit']
+        # market = sim_config.partition_config["market"]
+        # fee = sim_config.partition_config['feeRate']
+        # QL = sim_config.partition_config['quantityLimit']
+        # IL = sim_config.partition_config['inventoryLimit']
         # price_depth_limit = sim_config.partition_config['priceDepthLimit']
-        tick = sim_config.partition_config['tick']
+        # tick = sim_config.partition_config['tick']
         start = sim_config.partition_config["startTime"]
         end = sim_config.partition_config["endTime"]
         depth = sim_config.partition_config["depth"]
