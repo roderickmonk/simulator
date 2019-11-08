d2e3f6655a54a2ca978c44a721603b31f53bc0e3
roderickmonk <roderickmonk@gmail.com>
2019-11-08 00:47:40 +0000

    gitzap

--- Python/co2_validator.py
+++ Python/co2_validator.py
@@ -138,10 +138,10 @@ class Co2Validator:
                         buy_rate, sell_rate = self.trader.compute_orders(
                             buyob, sellob)
 
-                        print (type(buy_rate))
+                        print (type(sell_rate))
 
                         logging.error(
-                            "Elapsed Time: %f, buy_rate: %14.10f, sell_rate: %f",
+                            "Elapsed Time: %f, buy_rate: %14.10f, sell_rate: %14.10f",
                             timer(), buy_rate, sell_rate)
 
         except StopIteration:
