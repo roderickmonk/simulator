942d906ecfb607d78340274f6aaef9dc4dad45bb
roderickmonk <roderickmonk@gmail.com>
2019-11-08 00:57:45 +0000

    gitzap

--- Python/co2_validator.py
+++ Python/co2_validator.py
@@ -141,7 +141,7 @@ class Co2Validator:
 
                         logging.error(
                             "Elapsed Time: %f, best_buy: %14.10f, best_sell: %14.10f",
-                            timer(), buy_rates[0], sell_rates[0])
+                            timer(), list(buy_rates)[0], list(sell_rates)[0])
                         logging.error(
                             "Elapsed Time: %f, buy_rate: %14.10f, sell_rate: %14.10f",
                             timer(), buy_rate, sell_rate)
