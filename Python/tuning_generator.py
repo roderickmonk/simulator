9f5a494b9b7a4411962dcf692e54acd99b7a60bf
roderickmonk <roderickmonk@gmail.com>
2019-11-21 18:10:44 +0000

    gitzap

--- Python/tuning_generator.py
+++ Python/tuning_generator.py
@@ -340,7 +340,7 @@ if __name__ == '__main__':
         tg.config["name"],
         {
             # "ts": now,
-            "depths": tg.depths,
+            "depths": str(tg.depths),
             "price_depths": str(tg.price_depths),
             # "values": json.dumps(values),
         })
