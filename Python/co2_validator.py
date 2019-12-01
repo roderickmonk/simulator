c99a411da1b7116808617dd2c733edcf5e257a4e
roderickmonk <roderickmonk@gmail.com>
2019-12-01 17:19:14 +0000

    gitzap

--- Python/co2_validator.py
+++ Python/co2_validator.py
@@ -185,8 +185,8 @@ class Co2Validator:
                                 "Elapsed (msecs): %d\tBest Buy: %14.8f\tBest Sell: %14.8f\n"
                                 +
                                 "\t\t\t\t\t\t\tBuy Rate: %14.8f\tSell Rate: %14.8f\n" +
-                                "\t\t\t\t\t\t\tBuy Diff: %14.8f\tSell Diff: %14.8f\n",
-                                "\t\t\t\t\t\t\tBuy DCnt: %14.8f\tSell DCnt: %14.8f\n",
+                                "\t\t\t\t\t\t\tBuy Diff: %14.8f\tSell Diff: %14.8f\n" +
+                                "\t\t\t\t\t\t\tBuy DCnt: %14d\tSell DCnt: %14d\n",
                                 timer() * 1000, buy_rates[0], sell_rates[0],
                                 buy_rate, sell_rate, 
                                 buy_rate-rust_buy_rate, sell_rate-rust_sell_rate,
