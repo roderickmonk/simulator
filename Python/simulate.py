13d04d782a8f2c8d807c7a213ba8152ffdb4e157
roderickmonk <roderickmonk@gmail.com>
2021-09-26 21:45:30 +0000

    gitzap

--- Python/simulate.py
+++ Python/simulate.py
@@ -97,7 +97,7 @@ def simulate():
             raise Exception("Unable to Connect to Local MongoDB")
 
         partition_config = sim_db.partitions.find_one(
-            {"_id": partition_id}
+            {"_id": partition_id}, {"configName": False, "ts": False, "status": False}
         )
         if partition_config is None:
             raise RuntimeError("Unknown Trader Configuration")
