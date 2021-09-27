a5d8cda5a475700c92a3234a47020ec9c3521209
roderickmonk <roderickmonk@gmail.com>
2021-09-27 16:51:26 +0000

    gitzap

--- Python/load.py
+++ Python/load.py
@@ -183,8 +183,8 @@ if __name__ == "__main__":
             exchange=config["exchange"],
             market=config["market"],
             depth=config["depth"],
-            start=config["timeFrame"]["startTime"],
-            end=config["timeFrame"]["endTime"],
+            startTime=config["timeFrame"]["startTime"],
+            endTime=config["timeFrame"]["endTime"],
         )
 
         # Get first and last orderbook
